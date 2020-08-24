2a25523afea7ce4047b7ff0c50d3f58702327caa
Marta_Kurman <marta.klachko@gmail.com>
2020-08-24 23:26:28 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 
 
@@ -9,12 +10,15 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, StreamingQueryException {
 
       //  String path_2016 = "hdfs://sandbox-hdp.hortonworks.com:8020/201_expedia_output/ci_year=2016";
-        String path_2016 = "C:\\Users\\Marta_Kurman\\201_streaming_spark\\src\\main\\resources\\201_expedia_output\\ci_year=2016";
+        String path_2016 = args[0];
+        //"C:\\Users\\Marta_Kurman\\201_streaming_spark\\src\\main\\resources\\201_expedia_output\\ci_year=2016";
       //  String path_2017 = "hdfs://sandbox-hdp.hortonworks.com:8020/201_expedia_output/ci_year=2017";
-        String path_2017 = "C:\\Users\\Marta_Kurman\\201_streaming_spark\\src\\main\\resources\\201_expedia_output\\ci_year=2017";
+        String path_2017 =args[1];
+        //"C:\\Users\\Marta_Kurman\\201_streaming_spark\\src\\main\\resources\\201_expedia_output\\ci_year=2017";
        // String hotels_path = "hdfs://sandbox-hdp.hortonworks.com:8020/hotels";
       //  String hotels_weather_joined_path = "hdfs://sandbox-hdp.hortonworks.com:8020/hotels_weather_joined";
-        String hotels_weather_joined_path = "C:\\Users\\Marta_Kurman\\201_streaming_spark\\src\\main\\resources\\hotels_weather_joined";
+        String hotels_weather_joined_path = args[2];
+        //"C:\\Users\\Marta_Kurman\\201_streaming_spark\\src\\main\\resources\\hotels_weather_joined";
       //  String weather_path = "hdfs://sandbox-hdp.hortonworks.com:8020/weather";
 
 //
@@ -55,12 +59,20 @@ public class Main {
 //        Dataset<Row> data_2016_with_watermark = data_2016.withWatermark("lag_day", "2 hours");
 //        Dataset<Row> data_2017_with_watermark = data_2017.withWatermark("lag_day", "2 hours");
         //    Dataset<Row> hotels_weather_joined_with_watermark = hotels_weather_joined.withWatermark("_c13", "2 hours");
+        Dataset<Row> data  =  data_2016.union(data_2017);
+        data.writeStream()
+                .format("console")
+                .outputMode(OutputMode.Append())
+                .start()
+                .awaitTermination();
 
-
-//        Dataset<Row> joined =  expedia.as("e").join(hotels_weather_joined.as("h")) // INNER JOIN is the default
-//                .where("e.hotel_id = h._c0");
-
+//        Dataset<Row> data_joined =  data.as("d").join(hotels_weather_joined.as("h")) // INNER JOIN is the default
+//                .where("d.hotel_id = h._c0");
 //
+//
+//        Dataset<Row> data_joined_duration =data_joined.withColumn("duration", data_joined.col("srch_co")
+//                        .$minus(data_joined.col("srch_ci")));
+
 
     }
 }
