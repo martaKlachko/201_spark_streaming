151218b46d8ea3dd07eeda19963508f390dcad85
Marta_Kurman <marta.klachko@gmail.com>
2020-08-25 16:39:51 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -89,14 +89,15 @@ public class Main {
 
         Dataset<Row> data_joined_duration_1 = data_joined_duration
                 .withColumn("stay_type",
-                        functions.callUDF("sampleUDF", data_joined_duration.col("diff_days")))
+                        functions.callUDF("sampleUDF", data_joined_duration.col("diff_days")));
+        Dataset<Row> data_joined_duration_2=data_joined_duration_1
                 .groupBy(data_joined_duration.col("hotel_id"),data_joined_duration.col("stay_type") ).count();
 
 
         data_joined_duration_1.coalesce(1).writeStream()
                 .format("parquet")
                 .outputMode(OutputMode.Append())
-                .option("checkpointLocation", "/checkpoint9")
+                .option("checkpointLocation", "/checkpoint10")
                 .start("gs://spark_str/output")
                 .awaitTermination();
 
