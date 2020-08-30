e09cc314d51bfa7e7d8e1463730d07645f98e6c7
Marta_Kurman <marta.klachko@gmail.com>
2020-08-30 17:16:42 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,94 +1,32 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.api.java.UDF1;
-import org.apache.spark.sql.functions;
 import org.apache.spark.sql.streaming.StreamingQueryException;
-import org.apache.spark.sql.streaming.Trigger;
-import org.apache.spark.sql.types.DataTypes;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException, StreamingQueryException {
+    public static void main(String[] args) throws StreamingQueryException {
 
 
         String path_2016 = args[0];
         String path_2017 = args[1];
         String hotels_weather_joined_path = args[2];
+        String output_path = "gs://spark_str/output";
 
         SparkSession spark = SparkConfig.getSession();
-        Dataset<Row> data_2016 = spark
-                .readStream()
-                .format("parquet")
-                .parquet(path_2016);
+        Dataset<Row> data_2016 = TaskUtils.readParquet(spark, path_2016);
+        Dataset<Row> data_2017 = TaskUtils.readParquet(spark, path_2017);
 
-        Dataset<Row> data_2017 = spark
-                .readStream()
-                .format("parquet")
-                .parquet(path_2017);
+        Dataset<Row> hotels_weather_joined = TaskUtils.readCSV(spark, hotels_weather_joined_path);
+        TaskUtils.registerUDF(spark, "sampleUDF");
 
-        Dataset<Row> hotels_weather_joined = spark
-                .readStream()
-                .format("csv")
-                .option("header", "false")
-                .option("inferSchema", "true")
-                .csv(hotels_weather_joined_path);
+        Dataset<Row> data = TaskUtils.task1(spark, data_2016, data_2017, hotels_weather_joined);
 
-        spark.sqlContext()
-                .udf()
-                .register("sampleUDF", sampleUdf(), DataTypes.StringType);
-
-        Dataset<Row> data = data_2016.union(data_2017);
-
-
-        Dataset<Row> data_joined = data.select("id", "hotel_id", "srch_ci", "srch_co").as("d").join(hotels_weather_joined.as("h")) // INNER JOIN is the default
-                .where("d.hotel_id = h._c0");
-        data_joined.createOrReplaceTempView("data_joined");
-
-        Dataset<Row> data_joined_selected = spark.sql("select id, hotel_id, " +
-                "TO_DATE(CAST(UNIX_TIMESTAMP(srch_ci, 'yyyy-mm-dd') AS TIMESTAMP)) as srch_ci," +
-                " TO_DATE(CAST(UNIX_TIMESTAMP(srch_co, 'yyyy-mm-dd') AS TIMESTAMP)) as srch_co, _c7 as lat, _c8 as lng, _c11 as avg_tmpr_f , _c12 as avg_tmpr_c , _c13 as wthr_date from data_joined ");
-
-        Dataset<Row> data_joined_filtered = data_joined_selected.filter(data_joined_selected.col("avg_tmpr_f").$greater(0)
-                .or(data_joined_selected.col("avg_tmpr_c").$greater(0)));
-        data_joined_filtered.createOrReplaceTempView("data_joined_filtered");
-
-        Dataset<Row> data_joined_duration = spark.sql("SELECT *,DATEDIFF( srch_co, srch_ci ) AS diff_days  from data_joined_filtered");
-
-        Dataset<Row> data_joined_duration_1 = data_joined_duration
-                .withColumn("stay_type",
-                        functions.callUDF("sampleUDF", data_joined_duration.col("diff_days")));
-
-
-        data_joined_duration_1
-                .coalesce(1)
-                .writeStream()
-                .format("parquet")
-                .outputMode("append")
-                .option("checkpointLocation", "/checkpoint031")
-                .trigger(Trigger.ProcessingTime(5, TimeUnit.SECONDS))
-                .start("gs://spark_str/output")
-                .awaitTermination();
+        TaskUtils.writeStream(data, output_path);
 
 
     }
 
-    static UDF1<Integer, String> sampleUdf() {
-        return (s1) -> {
-            if (s1 == 1) {
-                return "Short_stay";
-            } else if (s1 >= 2 && s1 <= 7) {
-                return "Standart_stay";
-            } else if (s1 > 7 && s1 <= 14) {
-                return "Standart_extended stay";
-            } else if (s1 > 14 && s1 <= 30) {
-                return "Long_stay";
-            } else {
-                return "Erroneous_data";
-            }
-        };
-    }
+
 }
